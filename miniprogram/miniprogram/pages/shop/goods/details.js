79a4e484822a42862d992eadfa784518494e05d7
hurte@foxmail.com <cai521131>
2019-04-10 18:45:16 +0800

    update miniprogram

--- miniprogram/miniprogram/pages/shop/goods/details.js
+++ miniprogram/miniprogram/pages/shop/goods/details.js
@@ -2,77 +2,29 @@ const app = getApp();
 
 Page({
   data: {
-    selectData: [{
-        'index': 0,
-        'k': 'bkk',
-        'v': 'BKK - Suvarnabhumi'
-      },
-      {
-        'index': 1,
-        'k': 'dmk',
-        'v': 'DMK - Don Mueang'
-      },
-      {
-        'index': 2,
-        'k': 'hkt',
-        'v': 'HKT - Phuket'
-      },
-      {
-        'index': 3,
-        'k': 'cnx',
-        'v': 'CNX - Chiang'
-      },
-      // {
-      //   index: 0,
-      //   name: 'pos_select',
-      //   option: [
-      //     ('hotel', 'Hotel'),
-      //     ('hostel', 'Hostel'),
-      //     ('guesthouse', 'Guesthouse'),
-      //     ('private_property', 'Private Property'),
-      //   ]
-      // },
-    ],
+    airport: {
+      index: "",
+      option: [
+        ['bkk', 'BKK - Suvarnabhumi'],
+        ['dmk', 'DMK - Don Mueang'],
+        ['hkt', 'HKT - Phuket'],
+        ['cnx', 'CNX - Chiang'],
+      ]
+    },
+    pos_select: {
+      index: "",
+      option: [
+        ['hotel', 'Hotel'],
+        ['hostel', 'Hostel'],
+        ['guesthouse', 'Guesthouse'],
+        ['private_property', 'Private Property'],
+      ]
+    },
     images: {
       mode: 'scaleToFill',
       text: 'scaleToFill：不保持纵横比缩放图片，使图片完全适应',
-      src: ''
     },
-    passinfo: {
-      // 目的地
-      'destination': '',
-      'passport_img1': '',
-      'passport_img2': '',
-      'airimg1': '',
-      'airimg2': '',
-      'hotelimg': '',
-      'passport_type': '',
-      'nationality': '',
-      'airport': '',
-      'salutation': '',
-      'last_name': '',
-      'first_name': '',
-      'gender': '',
-      'birth_date': '',
-      'email': '',
-      'mobile_number': '',
-      'passport_number': '',
-      'passport_type': 'ordinary',
-      'passport_issue': '',
-      'passport_expiry': '',
-      'arrival_date': '',
-      'arrival_time': '',
-      'arrival_flightnum': '',
-      'departure_date': '',
-      'departure_time': '',
-      'departure_flightnum': '',
-      'pos_name': '',
-      'pos_select': '',
-      'pos_city_province': '',
-      'pos_district': '',
-      'pos_postcode': '',
-      'residential_address': '',
-    },
+
     StatusBar: app.globalData.StatusBar + 6,
     TabbarBot: app.globalData.tabbar_bottom,
     swiperlist: [
@@ -82,20 +34,28 @@ Page({
   formSubmit: function(e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
   },
-  bindPickerChange(e) {
-    console.log(e.detail)
-    console.log('picker ', e.detail.value)
-    // console.log('picker发送选择改变，携带值1为', e.currentTarget.dataset.index)
-    // this.setData({
-    //   airportIndex: e.detail.value
-    // })
+  PickerChange(e) {
+    let that = this;
+    let dic = e.currentTarget.id + ".index"
+    console.log(dic)
+    this.setData({
+      [dic]: e.detail.value,
+    })
   },
   bindDateChange(e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
+    console.log('picker发送选择改变，携带值为', e.detail.value, e.currentTarget, e.currentTarget.id)
     let d = e.detail.value.split("-")[2] + "/" + e.detail.value.split("-")[1] + "/" + e.detail.value.split("-")[0]
-    this.setData({
-      date: d
-    })
+    let dd;
+    if (e.currentTarget.id == 'id_arrival_date') {
+      dd = {
+        arrival_date: d
+      }
+    } else if (e.currentTarget.id == 'id_departure_date') {
+      dd = {
+        departure_date: d
+      }
+    }
+    this.setData(dd)
   },
   chooseImage(e) {
     console.log(e.currentTarget.dataset.name);
@@ -116,10 +76,10 @@ Page({
         // 限制最多只能留下3张照片
         // this.data.images = images.length <= 3 ? images : images.slice(0, 3)
         console.log(tempFilePaths);
+        let dd = classname + ".src"
         this.setData({
-          'images.src': tempFilePaths
+          [dd]: tempFilePaths
         })
-
         // TODO 上传图片
         wx.uploadFile({
           url: 'http://127.0.0.1:8000/upload/', // 仅为示例，非真实的接口地址
@@ -131,43 +91,31 @@ Page({
           },
           success(res) {
             console.log(res.data);
-            const data = JSON.parse(res.data);
+            let data = JSON.parse(res.data);
             console.log(data);
             console.log(data.filename);
             // do something
-            if (classname == 'passport_img1') {
+            if (classname == 'passport_img2') {
               that.setData({
-                'passinfo.passport_img1': data.filename,
-              })
-            } else if (classname == 'passport_img2') {
-              that.setData({
-                'passinfo.passport_img2': data.filename,
                 'passinfo.nationality': data.country.toLowerCase(),
                 'passinfo.last_name': data.last_name,
                 'passinfo.first_name': data.first_name,
                 'passinfo.salutation': data.sex,
                 'passinfo.gender': data.sex,
+                'passinfo.type': 'ordinary',
                 'passinfo.birth_date': data.birth_date,
                 'passinfo.passport_number': data.passport_no,
                 'passinfo.passport_issue': data.issue_date.slice(6, 8) + "/" + data.issue_date.slice(4, 6) + "/" + data.issue_date.slice(0, 4),
                 'passinfo.passport_expiry': data.expiry_date.slice(6, 8) + "/" + data.expiry_date.slice(4, 6) + "/" + data.expiry_date.slice(0, 4),
               })
-            } else if (classname == 'airimg1') {
-              that.setData({
-                'passinfo.airimg1': data.filename,
-              })
-
-            } else if (classname == 'airimg2') {
-              that.setData({
-                'passinfo.airimg2': data.filename,
-              })
-
-            } else if (classname == 'hotelimg') {
-              that.setData({
-                'passinfo.hotelimg': data.filename,
-              })
             }
-
+            // 设置文件预览
+            let dd = "passinfo."+ classname
+            console.log("dd--->",dd)
+            that.setData({
+              [dd]: data.filename,
+            })
+            
           }
         })
       }
@@ -175,11 +123,54 @@ Page({
   },
   bindTimeChange(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      time: e.detail.value
-    })
+    let dd;
+    if (e.currentTarget.id == 'departure_time') {
+      dd = {
+        departure_time: e.detail.value
+      }
+    } else if (e.currentTarget.id == 'arrival_time') {
+      dd = {
+        arrival_time: e.detail.value
+      }
+    }
+    this.setData(dd)
   },
   onLoad: function(options) {
     let that = this;
-  }
+  },
+  passinfo: {
+    // 目的地
+    'destination': '',
+    'passport_img1': '',
+    'passport_img2': '',
+    'airimg1': '',
+    'airimg2': '',
+    'hotelimg': '',
+    'passport_type': '',
+    'nationality': '',
+    'airport': '',
+    'salutation': '',
+    'last_name': '',
+    'first_name': '',
+    'gender': '',
+    'birth_date': '',
+    'email': '',
+    'mobile_number': '',
+    'passport_number': '',
+    'passport_type': 'ordinary',
+    'passport_issue': '',
+    'passport_expiry': '',
+    'arrival_date': '',
+    'arrival_time': '',
+    'arrival_flightnum': '',
+    'departure_date': '',
+    'departure_time': '',
+    'departure_flightnum': '',
+    'pos_name': '',
+    'pos_select': '',
+    'pos_city_province': '',
+    'pos_district': '',
+    'pos_postcode': '',
+    'residential_address': '',
+  },
 });
