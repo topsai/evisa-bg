e7b07f7f406914833c63bb2b51a3f7fd14cd6fb4
john <hurte@foxmail.com>
2019-05-20 00:36:22 +0800

    update celery

--- miniprogram/miniprogram/pages/shop/goods/details.js
+++ miniprogram/miniprogram/pages/shop/goods/details.js
@@ -83,7 +83,7 @@ Page({
         })
         // TODO 上传图片
         wx.uploadFile({
-          url: 'http://138.128.220.228:8000/upload/', // 仅为示例，非真实的接口地址
+          url: 'http://127.0.0.1:8000/upload/', // 仅为示例，非真实的接口地址
           filePath: tempFilePaths[0],
           name: 'file',
           formData: {
